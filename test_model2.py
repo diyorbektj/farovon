74c987436bf7d8230440dc20c106bfbe75512dfa
diyorbek <diyorbek5300@gmail.com>
2024-10-16 20:35:10 +0500

    fix port

--- test_model2.py
+++ test_model2.py
@@ -170,6 +170,6 @@ def video_feed():
 
 if __name__ == '__main__':
     try:
-        app.run(host='0.0.0.0', port=5000, debug=False)
+        app.run(host='0.0.0.0', port=5001, debug=False)
     finally:
         cv2.destroyAllWindows()
